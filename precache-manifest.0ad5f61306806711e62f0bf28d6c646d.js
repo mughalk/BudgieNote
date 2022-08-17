a68a9cdd1656acc35e417cc51e0115471fe487d2
Kashish <kashishmugha@gmail.com>
2022-08-17 14:14:19 -0400

    Updates

---
+++ precache-manifest.0ad5f61306806711e62f0bf28d6c646d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05ba84871b562100da6",
+    "url": "/mughalk/BudgieNote/static/css/main.6d9d83a4.chunk.css"
+  },
+  {
+    "revision": "e05ba84871b562100da6",
+    "url": "/mughalk/BudgieNote/static/js/main.525da30a.chunk.js"
+  },
+  {
+    "revision": "693a041a2b653f4f7bab",
+    "url": "/mughalk/BudgieNote/static/js/runtime~main.dd1da72a.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/mughalk/BudgieNote/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/mughalk/BudgieNote/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mughalk/BudgieNote/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d1786d08602eff7e6894a60135c17084",
+    "url": "/mughalk/BudgieNote/index.html"
+  }
+];
